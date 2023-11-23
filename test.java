3c215f58f1e5f3a298d27d142e2b53289c816b67
haya60 <117182026+haya60@users.noreply.github.com>
2023-11-23 19:54:03 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,23 @@
+public class test {
+    public static void main ( String [ ] args ) {
+
+   String MyName = "haya" ;
+   int age = 21 ;
+   double mark = 9.5 ;
+   String course = "Compiler" ;
+   // this is a comment
+
+ ~
+
+
+
+
+
+
+       
+
+
+    }
+
+  
+}
